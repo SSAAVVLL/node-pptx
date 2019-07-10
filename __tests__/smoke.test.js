a39346e671fbb913b263d9028bdd0c3883142b2c
SSAAVVLL <SSAAVVLL@bk.ru>
2019-07-10 10:21:53 +0500

    EVERETHING CHANGED

--- __tests__/smoke.test.js
+++
@@ -1,6 +0,0 @@
-const PPTX = require('../index.js');
-describe('test essentials', () => {
-    test('true', () => {
-        expect(Object.keys(PPTX)).toEqual(['Presentation', 'Composer', 'Slide', 'ShapeTypes', 'SchemeColors', 'LayoutTypes', 'BulletTypes']);
-    });
-});
